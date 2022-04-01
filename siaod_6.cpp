72268827026f7f268f5c646d6a1eee4f7782e0ce
khdepository <100402100+khdepository@users.noreply.github.com>
2022-04-01 09:54:53 +0300

    Create siaod_6.cpp

---
+++ siaod_6.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int hashxd(string x)
+{
+    int hashxd=x[0];
+    if (x.length()>=11){int hashxd=x[0]+x[1]+x[x.length()-1]+x[x.length()-5]+x[x.length()-9];}
+    else if (x.length()>=3){int hashxd=x[0]+x[1]+x[x.length()-1];}
+    return hashxd;
+}
+
+int main()
+{
+    ifstream file("text.txt");
+    int arr[100];
+    int i=0;
+    
+    while(i<=(sizeof arr / sizeof arr[0])) {
+        string str;
+        getline(file, str);
+        arr[i]=hashxd(str);
+        i++;
+    }
+    cout<<"key: ";
+    string key;
+    cin>>key;
+    
+    int keyhash=hashxd(key);
+    i=0;
+    while(i<=(sizeof arr / sizeof arr[0]))
+    {
+        if (keyhash==arr[i]){cout<<"string id is "<<i+1;break;}
+        i++;
+    }
+    
+    return 0;
+}
