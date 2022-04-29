af5449f09b6b62e4812f82a63d6d46f56abed715
khdepository <100402100+khdepository@users.noreply.github.com>
2022-04-29 10:26:25 +0300

    Update siaod_7.cpp

--- siaod_7.cpp
+++ siaod_7.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <limits.h>
 #include <string>
 #include <vector>
 
@@ -8,32 +7,112 @@ using namespace std;
 vector<int> digitsForA;
 vector<int> digitsForB;
 vector<int> result;
+int count=0;
 
-int arrayConvert(string str,vector<int> digits)
+int arrayConvert(string str)
 {
-    int is_negative=0;
-    if (str.length() == 0) {}
-        else {
-                if (str[0] == '-') {
-                        str = str.substr(1);
-                        is_negative=1;
-                }
-                for (long long i = str.length(); i > 0; i -= 1) {
-                    digits.push_back(atoi(str.substr(i - 1, 1).c_str()));
-                    cout<<digits.back()<<" ";
-                }
+    if(count!=1){
+        int i=0;
+        int is_negative=0;
+        if(str[0]=='-')
+        {
+            is_negative=1;
         }
+        while(i!=str.length())
+        {
+            digitsForA.push_back(str[i]);
+            i++;
+        }
+        if(is_negative==1)
+        {
+            digitsForA.push_back(-1);
+        }
+        count++;
+    }
+    else
+    {
+        int i=0;
+        int is_negative=0;
+        if(str[0]=='-')
+        {
+            is_negative=1;
+        }
+        while(i!=str.length())
+        {
+            digitsForB.push_back(str[i]);
+            i++;
+        }
+        if(is_negative==1)
+        {
+            digitsForB.push_back(-1);
+        }
+    }
     return 0;
 }
 
-int addition(){return 0;}
+int addition()
+{
+    int add=0;
+    int a_m=1,b_m=1;
+    int a=0;
+    int b=0;
+    if(!digitsForA.empty()){int a=digitsForA.back();}
+    if(!digitsForB.empty()){int b=digitsForB.back();}
+    if(a==-1)
+    {
+        digitsForA.pop_back();
+        a_m=-1;
+    }
+    if(b==-1)
+    {
+        digitsForB.pop_back();
+        b_m=-1;
+    }
+    int r=0;
+    while(max(digitsForA.size(),digitsForB.size())!=0)
+    {
+        a=0;
+        b=0;
+        add=0;
+        if(!digitsForA.empty()){a=digitsForA.back()*a_m-48;}
+        if(!digitsForB.empty()){b=digitsForB.back()*b_m-48;}
+        if(!digitsForA.empty()){digitsForA.pop_back();}
+        if(!digitsForB.empty()){digitsForB.pop_back();}
+        r=a+b+add;
+        if(r>9)
+        {
+            r=r-10;
+            add=1;
+        }
+        result.push_back(r);
+    }
+    if(add!=0)
+    {
+        result.push_back(add);
+    }
+    return 0;
+}
+
+int printVector()
+{
+    int i=result.size()-1;
+    while(i!=-1)
+    {
+        cout<<result[i];
+        i--;
+    }
+    return 0;
+}
 
 int main()
 {
     string a;
     getline(cin,a);
-    
-    arrayConvert(a, digitsForA);
-    arrayConvert(b, digitsForB);
+    string b;
+    getline(cin,b);
+    arrayConvert(a);
+    arrayConvert(b);
+    addition();
+    printVector();
     return 0;
 }
