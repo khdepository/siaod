7b4d330a62477087c867d43f42587435d92105bb
khdepository <100402100+khdepository@users.noreply.github.com>
2022-05-26 12:55:44 +0300

    Create siaod_12.cpp

---
+++ siaod_12.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int pos(const char* s, const char* p, int n)
+{
+	int i, j;
+	int lol, kek;
+
+	for (lol = 0; p[lol]; lol++);
+	for (kek = 0; s[kek]; kek++);
+
+	for (i = 0; i <= kek - lol; i++){
+		for (j = 0; s[i + j] == p[j]; j++);
+		if (j - lol == 1 && i == kek - lol && !(n - 1)) return i;
+		if (j == lol)
+			if (n - 1) n--;
+			else return i;
+	}
+	return -1;
+}
+int main() {
+	char x[30];char y[33];
+	int N, k, g;
+	int ar[30];
+	string line, a;
+	ifstream f("text.txt");
+	cout << "ASCII code\n : ";
+	k = 0;
+	while (f >> N) {
+		ar[k++] = N;
+	}
+	for (int i = 0; i < k; i++) {
+		cout << ar[i] << " ";
+	}
+	f.close();
+	cout << endl;
+	cout << "translation from: \n";
+	for (int i = 0; i < 30; i++) {
+		y[i] = ar[i];
+		cout << y[i] << "";
+	}
+	cout << endl;
+	ofstream out("binary.txt", ios::binary | ios::out);
+	out.write((char*)&y, sizeof y);
+	out.close();
+
+	ifstream in("text_2.txt", ios::binary | ios::in);
+	in.read((char*)&x, sizeof x);
+	in.close();
+	cout << "file data:\n";
+	cout << x << endl;
+	cout << "enter search number: \n";
+	cin >> a;
+	const char* s = x;
+	const char* p = a.c_str();
+	int i, n = 0;
+	for (i = 1; n != -1; i++){
+		n = pos(s, p, i);
+
+		if (n >= 0)
+			cout << n << endl;
+	}
+	cout << "after:\n ";
+	for (int i = 0; i <= 5; i++) {
+		for (int j = 0; j < 30; ++j) {
+			x[j] = x[j + 1];
+		}
+	}
+	for (int i = 0; i <= 30; i++) {
+		cout << x[i] << "";
+	}
+	return 0;
+}
