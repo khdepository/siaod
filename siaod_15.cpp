a089179e8e34ab1b71c6f379b1156818fb404b5c
khdepository <100402100+khdepository@users.noreply.github.com>
2022-05-26 13:08:52 +0300

    Create siaod_15.cpp

---
+++ siaod_15.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int MRT(int p[], int i, int j)
+{
+	if (i == j)
+    {
+ 		return 0;
+    }
+
+	int f;
+	int mini = INT_MAX;
+	int county;
+
+	for (f = i; f < j; f++)
+	{
+		county = MRT(p, i, f) + MRT(p, f + 1, j) + p[i - 1] * p[f] * p[j];
+
+		if (county < mini)
+        {
+			mini = county;
+        }
+
+	}
+
+	return mini;
+}
+
+int main()
+{
+	int arr[] = { 5,10,3,12,5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "return: "<< MRT(arr, 1, n - 1);
+
+	return 0;
+}
